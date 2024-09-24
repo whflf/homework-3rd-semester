f5f93e7cd3b7990d59e1d71e7dfa6353c38db5cc
whflf <fluffy.jesus@yandex.ru>
2024-09-24 20:09:51 +0300

    add the task files

---
+++ Lazy/LazyMultithread.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="LazyMultithread.cs" company="Elena Makarova">
+// Copyright (c) Elena Makarova. All rights reserved.
+// </copyright>
+
+namespace Lazy;
+
+/// <summary>
+/// Provides a thread-safe implementation of lazy initialization.
+/// </summary>
+/// <typeparam name="T">The type of the value to be lazily initialized.</typeparam>
+public class LazyMultithread<T>(Func<T> supplier) : ILazy<T>
+{
+    private readonly Func<T> Supplier = supplier;
+    private readonly object LockObject = new object();
+    private T? _value;
+    private bool _valueIsCalculated = false;
+
+    /// <summary>
+    /// Gets the lazily initialized value.
+    /// The first call will invoke the supplier, while subsequent calls will return the cached value.
+    /// </summary>
+    /// <returns>The lazily initialized value of type <typeparamref name="T"/>.</returns>
+    public T? Get()
+    {
+        if (!this._valueIsCalculated)
+        {
+            lock (this.LockObject)
+            {
+                if (!this._valueIsCalculated)
+                {
+                    this._value = this.Supplier();
+                    this._valueIsCalculated = true;
+                }
+            }
+        }
+
+        return this._value;
+    }
+}
---
+++ Lazy/LazySingleThread.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="LazySingleThread.cs" company="Elena Makarova">
+// Copyright (c) Elena Makarova. All rights reserved.
+// </copyright>
+
+namespace Lazy;
+
+/// <summary>
+/// Provides a single-thread implementation of lazy initialization.
+/// </summary>
+/// <typeparam name="T">The type of the value to be lazily initialized.</typeparam>
+public class LazySingleThread<T>(Func<T> supplier) : ILazy<T>
+{
+    private readonly Func<T> Supplier = supplier;
+    private T? _value;
+    private bool _valueIsCalculated = false;
+
+    /// <summary>
+    /// Gets the lazily initialized value.
+    /// The first call will invoke the supplier, while subsequent calls will return the cached value.
+    /// </summary>
+    /// <returns>The lazily initialized value of type <typeparamref name="T"/>.</returns>
+    public T? Get()
+    {
+        if (!this._valueIsCalculated)
+        {
+            this._value = this.Supplier();
+            this._valueIsCalculated = true;
+        }
+
+        return this._value;
+    }
+}
