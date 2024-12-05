4d8186f7584aea83a54111713ab996fecd6b8679
whflf <fluffy.jesus@yandex.ru>
2024-12-05 18:55:52 +0300

    add task files

---
+++ Reflector.Tests/ReflectorTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Reflector.Tests;
+
+public class ReflectorTests
+{
+    [Test]
+    public void TestNotClass()
+    {
+        Assert.Throws<ArgumentException>(() => Reflector.PrintStructure(typeof(TestStruct)));
+    }
+
+    [Test]
+    public void TestPrintSimpleClass()
+    {
+        Reflector.PrintStructure(typeof(SimpleClass));
+        var actual = File.ReadAllText("SimpleClass.cs");
+        var expected = File.ReadAllText("TestSimpleClass.txt");
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void TestDiffClasses()
+    {
+        var result = Reflector.DiffClasses(typeof(SimpleClass), typeof(SecondSimpleClass));
+        var expected = "Fields differences: number boolean \nMethods differences: ReturnInt ReturnString ";
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
---
+++ Reflector.Tests/TestStruct.cs
@@ -0,0 +1,7 @@
+﻿namespace Reflector.Tests;
+
+internal struct TestStruct
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
